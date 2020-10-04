aae35f5752bb253063169ee4c43624a07dc97fc8
juanmora <juanmora.lpz18@gmail.com>
2020-10-04 17:04:43 -0500

    Hola-Mundo

---
+++ Hola-Mundo/hola-mundo.py
@@ -0,0 +1,20 @@
+#Hola Mundo en Python
+
+print('Hola Mundo')
+
+palabra= 'Hola Mundo'
+print(palabra)
+
+print(type(palabra))
+
+a=5
+b=10
+print(a+b)
+
+
+print('\n--Tabla de Multiplicar del 0 al 10--')
+x=11
+for i in range(x):
+    print('\n')
+    for j in range(x): 
+        print i,'*',j,'= ',i*j
---
+++ Hola-Mundo/html-css-js/script.js
@@ -0,0 +1,4 @@
+let mensaje;
+
+mensaje=document.getElementById('mensaje')
+mensaje.innerHTML='Hola Mundo con JS :)';
